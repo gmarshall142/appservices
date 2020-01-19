93ca1b332abe35fdb3528f1b9bd3fe0ba1cfda54
Geoff Marshall <gmarshall142@gmail.com>
2020-01-19 12:25:06 -0500

    added endpoint for creating and authenticating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class AdminSiteTests(TestCase):
 	def setUp(self):
 		self.client = Client()
-		self.admin_user = get_user_model().objects.create_super_user(
+		self.admin_user = get_user_model().objects.create_superuser(
 			email='admin@londonappdev.com',
 			password='password123'
 		)
